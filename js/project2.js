c5fd218c9327d27381ef4b0905dec539a1fc64cd
barreranoemi <32409770+barreranoemi@users.noreply.github.com>
2018-01-17 16:04:29 -0800

    Add files via upload

--- js/project2.js
+++ js/project2.js
@@ -1,37 +1,59 @@
 "use strict";
 
-//Constant variables 
+// Constants needed
 const pageList = document.querySelector('ul');
 const eachStudent = pageList.children;
 const buttonDiv = document.querySelector('.pagination');
 const buttonUl = buttonDiv.querySelector('ul');
 const studentsPerPage = 10;
-const searchDiv = document.querySelector('.student-search');
-const noResultDiv = document.querySelector('.no-results');
 var pagesNumbersList;
 var studentList = document.getElementsByClassName('student-list')[0];
 var studentNumber = studentList.children.length;
+const pageHeaderDiv = document.querySelector('.page-header');
 
-//Hide all but the first 10 students in the list.
-function showFirstTen() {
-for (let i = 0; i < eachStudent.length; i++) {
-    if (i < studentsPerPage) {
-        eachStudent[i].style.display = '';
-    } else {
-        eachStudent[i].style.display = 'none';
-    }
-}
-var a = document.querySelectorAll('ul li a');
-a[0].classList.add('active');
+var searchButton = document.createElement('button');
+searchButton.setAttribute('class', 'student-search')
+var searchInput = document.createElement('input');
+searchInput.setAttribute('placeholder', 'search up Students');
+var noResult = document.createElement('div');
+noResult.setAttribute('class', 'noResultDiv');
+pageHeaderDiv.appendChild(noResult);
+
+const searchDiv = document.querySelector('.student-search');
+const noResultDiv = document.querySelector('.noResultDiv');
+
+function showSearch() {
+searchButton.textContent = 'Search';
+searchButton.setAttribute('placeholder', 'search up Students...');
+pageHeaderDiv.appendChild(searchButton);
+pageHeaderDiv.appendChild(searchInput);
 }
 
-//How to calculate the number of pages needed.
+var div = document.createElement('div');
+ document.body.appendChild(div);
+
+// Determining the amount of pages needed based on the number of students
 function amountOfPages() {
     let pages = Math.ceil(eachStudent.length / studentsPerPage);
     return pages;
 }
 
-//When a user clicks on the other pages, next 10 student should be shown.
+// Function to show first ten students
+function showFirstTen() {
+    for (let i = 0; i < eachStudent.length; i++) {
+        if (i < studentsPerPage) {
+            eachStudent[i].style.display = '';
+        } else {
+            eachStudent[i].style.display = 'none';
+        }
+    }
+    var a = document.querySelectorAll('ul li a');
+    a[0].classList.add('active');
+
+
+}
+
+// Loop to create page links based on number of pages needed
 for (let i = 1; i <= amountOfPages(); i++) {
     let pageli = document.createElement('li');
     let pageLink = document.createElement('a');
@@ -44,16 +66,19 @@ for (let i = 1; i <= amountOfPages(); i++) {
     pagesNumbersList.forEach(activePage, eachStudent);
 }
 
+
 // How to get the links to be active
 function activePage(item){
     item.onclick = function(){
-        for(var i=0; i<studentNumber; i++){
+         for(var i=0; i<studentNumber; i++){
             studentList.children[i].style.display = 'none';
         }
-    	for(var i=0; i<pagesNumbersList.length; i++){
+
+        for(var i=0; i<pagesNumbersList.length; i++){
             pagesNumbersList[i].classList.remove('class', 'active');
         }
-    	item.classList.add('active');
+
+        item.classList.add('active');
         var pageNumber = parseInt(item.innerHTML);
         var start = (pageNumber * 10) - 10;
         var end = (pageNumber * 10);
@@ -63,7 +88,7 @@ function activePage(item){
     }
 }
 
-//Event listener used to divide students among the pages
+// Using event listener to divide students among the pages
 buttonDiv.addEventListener('click', (event) => {
     noResultDiv.innerHTML = '';
     let buttonNumber = parseInt(event.target.textContent);
@@ -78,15 +103,15 @@ buttonDiv.addEventListener('click', (event) => {
     }
 });
 
-//Search component so that a user could search for a particular student or students
-let searchInput = document.createElement('input');
-let searchButton = document.createElement('button');
-function showSearch() {
-    searchInput.placeholder = 'Search up students...';
-    searchButton.textContent = 'Search';
-    searchDiv.appendChild(searchInput);
-    searchDiv.appendChild(searchButton);
-}
+// // How to display the search box dynamically
+// let searchInput = document.createElement('input');
+// let searchButton = document.createElement('button');
+// function showSearch() {
+//     searchInput.placeholder = 'Search up students...';
+//     searchButton.textContent = 'Search';
+//     searchDiv.appendChild(searchInput);
+//     searchDiv.appendChild(searchButton);
+// }
 
 // Event listener for search box
 // Array to hide students
@@ -97,19 +122,23 @@ searchButton.addEventListener('click', () => {
     for (let i = 0; i < eachStudent.length; i++) {
         if (eachStudent[i].innerHTML.indexOf(filter) > -1) {
             eachStudent[i].style.display = '';
-    	} else {
+
+        } else {
             eachStudent[i].style.display = 'none';
             searchResults.push(i);
         }
     }
-
     // If no students match the search results, a 'student not found' message is displayed
     if (searchResults.length === eachStudent.length) {
         noResultDiv.innerHTML = '<h1>Student Not Found</h1>';
+        
     } else {
         noResultDiv.textContent = '';
     }
 });
+
+
+
 
 // Function call to display first ten students on load
 showFirstTen();
